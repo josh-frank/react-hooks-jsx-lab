e9b41b2786164a46bc9e0abe3b009dae919673e3
Josh Frank <eurosewusa@gmail.com>
2021-02-21 22:37:00 -0500

    commit tree 23568ccd2cb69395086f380e4cc000e69e630651

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I don't want no scrub - scrub is a man who ain't gettin' no love from me</p>
+      <img src={ image } alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
